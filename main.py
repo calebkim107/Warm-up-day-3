f5a4ea9ddb110adef0bb38de2bd71d3b84fa88ba
calebkim107 <calebkim1117@gmail.com>
2020-08-05 19:24:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# - something that can store
+# information
+name = "Spongebob"
+score = 0
+height = 62.5
+# Boolean data type
+game_over = True
+
+
+# LOOPS - repeats code
+
+# for Loop - repeats code for a given amount of times
+
+for i in range(4):
+  print("Remmy boy")
+
+# while loop - repeats code until condition is met aka turns false
+count = 0
+while count < 3:
+  print("Remmy boi", count)
+  count += 1
+  
